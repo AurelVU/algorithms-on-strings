dd0ee8ddd3601a3e069c3815f95c41cb374b73bc
Vladimir <33979689+AurelVU@users.noreply.github.com>
2021-02-13 10:36:15 +0300

    Исправлена ошибка во 2 задании

--- task2.py
+++ task2.py
@@ -1,18 +1,20 @@
 def solver(input_string):
     max_gran = 0
     for len_of_gran in range(1, len(input_string)):
-        current_char = 0
-        while current_char < len_of_gran:
-            if input_string[current_char] == input_string[len(input_string) - len_of_gran + current_char]:
-                current_char += 1
-            elif current_char != len_of_gran:
-                max_gran = len_of_gran
+        current_count = 0
+        while current_count < len_of_gran and input_string[current_count] == input_string[len(input_string) - len_of_gran + current_count]:
+            current_count += 1
+        if current_count == len_of_gran:
+            max_gran = len_of_gran
     return max_gran
 
 
 
-input_string = 'testfdfshjdfs_testh_testf_testdh_testf_testkgfdhdfkg_test_jgfkdfltest'
+input_string = 'test_fdfshjdfs_testh_testf_testdh_testf_testkgfdhdfkg_test_jgfkdfl_test'
 print('Максимальная длина грани: ', solver(input_string))
 
 input_string = 'test'
+print('Максимальная длина грани: ', solver(input_string))
+
+input_string = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 print('Максимальная длина грани: ', solver(input_string))
