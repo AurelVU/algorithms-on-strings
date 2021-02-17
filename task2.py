3709b0a140269f0a3b060963ac9e649824d7a96b
Владимир <eau120676@yandex.ru>
2021-02-17 11:42:57 +0300

    Проведена оптимизация

--- task2.py
+++ task2.py
@@ -1,12 +1,11 @@
 def solver(input_string):
-    max_gran = 0
-    for len_of_gran in range(1, len(input_string)):
+    for len_of_gran in reversed(range(1, len(input_string))):
         current_count = 0
         while current_count < len_of_gran and input_string[current_count] == input_string[len(input_string) - len_of_gran + current_count]:
             current_count += 1
         if current_count == len_of_gran:
-            max_gran = len_of_gran
-    return max_gran
+            return len_of_gran
+    return 0
 
 
 
